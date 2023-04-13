54b92e9ed16757a3093e2c5648550820b0a1e0a6
tom-lv <tomlv.dev@gmail.com>
2023-04-13 18:21:33 +0100

    Optimisation: remove fxml file

--- src/main/java/com/tomaslevesconte/javachess/ChessApplication.java
+++ src/main/java/com/tomaslevesconte/javachess/ChessApplication.java
@@ -1,20 +1,20 @@
 package com.tomaslevesconte.javachess;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class ChessApplication extends Application {
 
     public final static double BOARD_SIZE = 850.0;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("chess-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), BOARD_SIZE, BOARD_SIZE);
+    public void start(Stage stage) {
+        AnchorPane anchorPane = new AnchorPane();
+        new PieceBuilder(new Chessboard(ChessApplication.BOARD_SIZE, anchorPane));
+        Scene scene = new Scene(anchorPane, BOARD_SIZE, BOARD_SIZE);
         stage.setTitle("Chess");
         stage.setResizable(false);
         stage.setScene(scene);
