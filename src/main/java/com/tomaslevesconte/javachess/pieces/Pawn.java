be81ef0d036a8f3884986d812195f5631b91b0e6
tom-lv <tomlv.dev@gmail.com>
2023-05-01 18:37:39 +0100

    Refactor code: create attack/move patterns & completely change implementation

--- src/main/java/com/tomaslevesconte/javachess/pieces/Pawn.java
+++ src/main/java/com/tomaslevesconte/javachess/pieces/Pawn.java
@@ -20,47 +20,52 @@ public class Pawn extends Piece {
     @Override
     public ArrayList<Square> getLegalMoves() {
         ArrayList<Square> legalMoves = new ArrayList<>();
-        double squareSize = getChessboard().getSquareSize();
-        double multiplier = getPieceColour().equals(PieceColour.WHITE) ? -squareSize : squareSize;
 
-        // Evaluate y coordinate up/down squares (depending on pieceColour)
-        double nextY = (getCurrentY() + multiplier);
-        for (int i = 0; i < getSquaresItCanMove(); i++) {
-            if (!hasMoved() && !getChessboard().isSquareOccupied(getCurrentX(), nextY)) {
-                legalMoves.add(Square.findSquare(getCurrentX(), nextY, squareSize));
-            } else if (getCurrentY() == 0 ||  getCurrentY() == (squareSize * 7)) {
-                break;
-            } else if (!getChessboard().isSquareOccupied(getCurrentX(), nextY)) {
-                legalMoves.add(Square.findSquare(getCurrentX(), nextY, squareSize));
-                break;
-            } else {
-                break;
-            }
-            nextY += multiplier;
-        }
-
-        // Evaluate x coordinate down (<--) for capturing
-        double[] nextDiagonal = {(getCurrentX() - squareSize), (getCurrentY() + multiplier)};
-        if (getCurrentX() == 0
-                || getPieceColour().equals(PieceColour.WHITE) && getCurrentY() == 0
-                || getPieceColour().equals(PieceColour.BLACK) && getCurrentY() == (squareSize * 7)) {
-            // Do nothing
-        } else if (getChessboard().isSquareOccupied(nextDiagonal[0], nextDiagonal[1])
-                && getPieceColour() != getChessboard().getPiece(nextDiagonal[0], nextDiagonal[1]).getPieceColour()) {
-            legalMoves.add(Square.findSquare(nextDiagonal[0], nextDiagonal[1], squareSize));
-        }
-
-        // Evaluate x coordinate up (-->) for capturing
-        nextDiagonal = new double[]{(getCurrentX() + squareSize), (getCurrentY() + multiplier)};
-        if (getCurrentX() == (squareSize * 7)
-                || getPieceColour().equals(PieceColour.WHITE) && getCurrentY() == 0
-                || getPieceColour().equals(PieceColour.BLACK) && getCurrentY() == (squareSize * 7)) {
-            // Do nothing
-        } else if (getChessboard().isSquareOccupied(nextDiagonal[0], nextDiagonal[1])
-                && getPieceColour() != getChessboard().getPiece(nextDiagonal[0], nextDiagonal[1]).getPieceColour()) {
-            legalMoves.add(Square.findSquare(nextDiagonal[0], nextDiagonal[1], squareSize));
-        }
+        legalMoves.addAll(getPawnMovePatterns()); // Add move patterns
+        legalMoves.addAll(getPawnAttackPatterns()); // Add attack patterns
 
         return legalMoves;
+    }
+
+    private ArrayList<Square> getPawnMovePatterns() {
+        ArrayList<Square> movePatterns = new ArrayList<>();
+
+        double squareSize = getChessboard().getSquareSize();
+        // Pawns move in different directions depending on colour
+        double multiplier = getPieceColour().equals(PieceColour.WHITE) ? -squareSize : squareSize;
+
+        // Every pawn move pattern
+        movePatterns.add(Square.find(getCurrentX(), (getCurrentY() + multiplier), squareSize));
+        movePatterns.add(Square.find(getCurrentX(), (getCurrentY() + (multiplier * 2)), squareSize));
+
+        // Remove if square !exist, or if square is occupied, or if square == 2nd square and pawn has moved
+        movePatterns.removeIf(moveSquare -> (moveSquare == null
+                || getChessboard().isSquareOccupied(moveSquare))
+                || moveSquare.equals(movePatterns.get(1))
+                && hasMoved());
+
+        System.out.println(movePatterns);
+
+        return movePatterns;
+    }
+
+    private ArrayList<Square> getPawnAttackPatterns() {
+        ArrayList<Square> attackPatterns = new ArrayList<>();
+
+        double squareSize = getChessboard().getSquareSize();
+        // Pawns move in different directions depending on colour
+        double multiplier = getPieceColour().equals(PieceColour.WHITE) ? -squareSize : squareSize;
+
+        // Every pawn attack pattern
+        attackPatterns.add(Square.find((getCurrentX() - squareSize), (getCurrentY() + multiplier), squareSize));
+        attackPatterns.add(Square.find((getCurrentX() + squareSize), (getCurrentY() + multiplier), squareSize));
+
+        // Remove if square !exist, or if square is !occupied, or if square is occupied by the same colour
+        attackPatterns.removeIf(attackSquare -> (attackSquare == null // If null (out of bounds)
+                || !getChessboard().isSquareOccupied(attackSquare)
+                || getChessboard().isSquareOccupied(attackSquare)
+                && getChessboard().getPiece(attackSquare).getPieceColour().equals(getPieceColour())));
+
+        return attackPatterns;
     }
 }
